a7f50d279dabf2ccdfdf7aa6954c754e46d093f8
umer <bscs23211@itu.edu.pk>
2025-01-12 00:26:25 +0500

    refactor: change to block cursor

--- init.lua
+++ init.lua
@@ -1,3 +1,15 @@
 require ("key_maps")
 require ("plugins")
 require ("plugs_config")
+
+--to fix input delay when ada_language_server is active
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "ada",
+    callback = function()
+        vim.api.nvim_buf_del_keymap(0, "i", "<leader>aj")
+        vim.api.nvim_buf_del_keymap(0, "i", "<leader>al")
+    end,
+})
+
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
+vim.opt.termguicolors = true
