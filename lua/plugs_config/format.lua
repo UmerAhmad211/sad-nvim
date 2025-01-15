8a0ff31da43443641cd3e61b50a7ba3a3cb4b581
umer <bscs23211@itu.edu.pk>
2025-01-15 22:41:43 +0500

    feat: add format

---
+++ lua/plugs_config/format.lua
@@ -0,0 +1,14 @@
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		rust = { "rustfmt", lsp_format = "fallback" },
+		sh = { "shfmt" },
+		clangf = { "clang_format" },
+		python = { "ruff" },
+	},
+	format_on_save = {
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 500,
+	},
+})
