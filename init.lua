f5a3cad618164087308d30b822e885fd943aa387
umer <bscs23211@itu.edu.pk>
2025-04-15 08:10:33 +0500

    feat: use 4 spaces tab for all other langs, use 8 spaces tab for c family langs

--- init.lua
+++ init.lua
@@ -1,15 +1,32 @@
-require ("key_maps")
-require ("plugins")
-require ("plugs_config")
+require("key_maps")
+require("plugins")
+require("plugs_config")
 
 --to fix input delay when ada_language_server is active
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "ada",
-    callback = function()
-        vim.api.nvim_buf_del_keymap(0, "i", "<leader>aj")
-        vim.api.nvim_buf_del_keymap(0, "i", "<leader>al")
-    end,
+	pattern = "ada",
+	callback = function()
+		vim.api.nvim_buf_del_keymap(0, "i", "<leader>aj")
+		vim.api.nvim_buf_del_keymap(0, "i", "<leader>al")
+	end,
 })
 
 vim.opt.guicursor = "n-v-i-c:block-Cursor"
 vim.opt.termguicolors = true
+
+-- def 4 spaces tab
+vim.opt.expandtab = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 0
+
+-- c family langs, use 8 spaces tab
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+	pattern = { "*.c", "*.h", "*.cpp", "*.hpp" },
+	callback = function()
+		vim.opt.expandtab = false
+		vim.opt.tabstop = 8
+		vim.opt.shiftwidth = 8
+		vim.opt.softtabstop = 0
+	end,
+})
